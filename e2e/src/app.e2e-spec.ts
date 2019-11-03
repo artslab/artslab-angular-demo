b822556116bfaf8a16137f1085c69202ce421cd8
artslab <dev@artslab.info>
2019-11-03 10:47:37 +0100

    refactor: rename application

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('artslabActions app is running!');
+    expect(page.getTitleText()).toEqual('artslabDemo app is running!');
   });
 
   afterEach(async () => {
